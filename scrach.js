f883501a79b302de32a6ad36aeda192ab97f7107
RAJU <pydiraju4262@gmail.com>
2024-07-26 10:41:56 +0530

    Delete scrach.js

--- scrach.js
+++
@@ -1,9 +0,0 @@
-let navbar = document.querySelector('.header.flex.navbar');
-
-
-var menubtn =document.getElementById('menu-btn')
-menubtn.addEventListener('click',()=>{
-    var mynav = document.getElementsByClassName("navbar")[0]
-    mynav.classList.toggle("active")
-   
-})
